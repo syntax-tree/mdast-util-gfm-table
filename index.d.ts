2bcc6fcc456eb945d2ba38df88a7ce55b1e03923
Titus Wormer <tituswormer@gmail.com>
2023-01-30 10:40:49 +0100

    Refactor types to use `CompileData` interface

--- index.d.ts
+++ index.d.ts
@@ -2,6 +2,17 @@ export type {Options} from './lib/index.js'
 
 export {gfmTableFromMarkdown, gfmTableToMarkdown} from './lib/index.js'
 
+// Add custom data tracked to turn markdown into a tree.
+declare module 'mdast-util-from-markdown' {
+  // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+  interface CompileData {
+    /**
+     * Whether we’re currently in a table.
+     */
+    inTable?: boolean | undefined
+  }
+}
+
 // Add custom data tracked to turn a syntax tree into markdown.
 declare module 'mdast-util-to-markdown' {
   // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
